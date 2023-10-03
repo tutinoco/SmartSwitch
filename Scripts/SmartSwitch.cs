64cb0436985ff1ff9bdd9c0c12c2c1431b66a08a
tutinoco <m@tutino.co>
2023-10-04 00:57:59 +0900

    SimpleNetworkのグループ名が廃止された変更に対応
    SimpleNetworkInit()の第一引数を削除しました。
    groupNameプロパティの名前をgroupに変更し、ヘッダーテキストにワイルドカードが指定できる説明を追加しました。

--- Scripts/SmartSwitch.cs
+++ Scripts/SmartSwitch.cs
@@ -26,76 +26,87 @@ namespace tutinoco
 
     public class SmartSwitch : SimpleNetworkBehaviour
     {
+        [System.NonSerialized] public bool isOn;
+
         [Header("スイッチの種類を設定")]
         public SwitchType type;
 
+        [Header("スイッチの初期状態を設定")]
+        public bool defaultOn;
+
         [Header("スイッチON/OFF時に有効にするオブジェクトを設定")]
-        public bool isGlobalSwitch;
+        public bool globalActiveSwitch;
         public string activeGroupOnSwitch;
-        public GameObject[] activeObjectsOnSwitch;
         public string activeGroupOffSwitch;
+        public GameObject[] activeObjectsOnSwitch;
         public GameObject[] activeObjectsOffSwitch;
 
         [Header("スイッチ切り替え時に鳴らす音を設定")]
-        public bool isGlobalSound;
-        public AudioSource audioSource;
+        public bool globalSound;
         public AudioClip onSound;
         public AudioClip offSound;
-
-        [Header("このスイッチのグループ名をを設定")]
-        public string groupName;
+        public bool playParticleWithSound;
 
         [Header("スイッチON/OFF時に実行するイベントを設定")]
-        public bool isGlobalEvent;
-        public string onEventName;
+        public bool globalEvent;
         public string onEventTarget;
+        public string onEventName;
         public string[] onEventValues;
         private object[] onEventArgs;
-        public string offEventName;
         public string offEventTarget;
+        public string offEventName;
         public string[] offEventValues;
         private object[] offEventArgs;
 
-        [Header("スイッチの初期状態を設定（チェックすると最初からONになります）")]
-        public bool isDefaultOn;
-        [System.NonSerialized] public bool isOn;
-
-        [Header("押したスイッチを戻すタイマー設定（0なら無効、30なら30フレーム後に戻ります）")]
-        public int switchBackFrame;
-        private int switchBackCount;
-        private bool defaultState;
-
-        [Header("スイッチのコライダーを設定（省略すると自動的に取得します）")]
-        public Collider collider;
+        [Header("グループ化するスイッチの名前を指定（ワイルドカード使用可）")]
+        public string group;
 
         [Header("他のスイッチとリンクして動作させるスイッチを設定")]
         public LinkType linkType;
         public bool enableAutoLink;
-        public bool executeLinkedEvent;
+        public bool enableLinkedActiveGroup;
+        public bool enableLinkedSound;
+        public bool enableLinkedEvent;
+
+        [Header("押したスイッチを自動で戻すタイマー（0なら無効、30なら30フレーム後に戻ります）")]
+        public int switchBackFrame;
+        private int switchBackCount;
+        private bool defaultState;
 
         [Header("スイッチを操作できる人を登録（空なら誰でも操作できます）")]
         public string[] memberships;
+
+        [Header("コンポーネントを手動で設定（省略すると自動的に取得します）")]
+        public AudioSource audioSource;
+        public ParticleSystem particle;
+        public Collider collider;
 
         private bool isInArea;
         private bool isTrigger;
 
         void Start()
         {
-            isOn = isDefaultOn;
-            SimpleNetworkInit(groupName);
-            if( !collider ) collider = GetComponent<Collider>();
-            if( type==SwitchType.Button && switchBackFrame==0 ) switchBackFrame=8;
+            isOn = defaultOn;
+            SimpleNetworkInit();
+
+            if( collider == null ) collider = GetComponent<Collider>();
+            if( audioSource == null ) audioSource = GetComponent<AudioSource>();
+            if( particle == null ) particle = GetComponent<ParticleSystem>();
+
+            if( type==SwitchType.Button && switchBackFrame<=0 ) switchBackFrame=8;
 
             onEventArgs = new object[onEventValues.Length];
             for(var i=0; i<onEventArgs.Length; i++) onEventArgs[i] = Parse(onEventValues[i]);
+            if( onEventArgs.Length == 0 ) onEventArgs = none;
 
-            offEventArgs = new object[offEventValues.Length];
+            offEventArgs = new object[offEventValues.Length];            
             for(var i=0; i<offEventArgs.Length; i++) offEventArgs[i] = Parse(offEventValues[i]);
+            if( offEventArgs.Length == 0 ) offEventArgs = none;
         }
 
         void Update()
         {
-            if( type == SwitchType.Button ) DisableInteractive = isDefaultOn!=isOn;
+            if( type == SwitchType.Button ) DisableInteractive = defaultOn!=isOn;
             if( type == SwitchType.Pickup || type == SwitchType.ToggleTrigger || type == SwitchType.PushTrigger || type == SwitchType.Join ) DisableInteractive = true;
 
             if( switchBackCount >= 0 ) switchBackCount--;
@@ -138,14 +149,12 @@ namespace tutinoco
 
         public override void OnPlayerJoined( VRCPlayerApi player )
         {
-            if( Networking.IsOwner(gameObject) && type != SwitchType.Join ) return;
-            Switch(true);
+            if( Networking.IsOwner(gameObject) && type == SwitchType.Join ) Switch(true);
         }
 
         public override void OnPlayerLeft( VRCPlayerApi player )
         {
-            if( Networking.IsOwner(gameObject) && type != SwitchType.Join ) return;
-            Switch(false);
+            if( Networking.IsOwner(gameObject) && type == SwitchType.Join ) Switch(false);
         }
 
         private void UpdateObjects()
@@ -158,13 +167,13 @@ namespace tutinoco
 
         public void UpdateLinks()
         {
-            foreach( var b in GetBehaviours(groupName) ) {
+            foreach( var b in GetBehaviours(group) ) {
                 var ss = b.gameObject.GetComponent<SmartSwitch>(); // isがつかえない？
                 if( ss == null || ss == this ) continue;
                 if( linkType==LinkType.Sync && isOn!=ss.IsOn() ) ss.ExecEvent("_Sync", isOn);
                 if( linkType==LinkType.Radio && ss.IsOn() ) ss.ExecEvent("_Sync", false);
                 if( linkType==LinkType.Pulse ) ss.ExecEvent("_Sync", !ss.IsOn());
-                if( executeLinkedEvent ) ss.ExecEvent("_Exec");
+                if( enableLinkedEvent ) ss.ExecEvent("_Exec");
             }
         }
 
@@ -191,10 +200,11 @@ namespace tutinoco
             if( switchBackCount==-1 && isOn!=flg ) switchBackCount = switchBackFrame;
             isOn = flg;
 
-            var syncSendto = isGlobalSwitch ? SendTo.All : SendTo.Self;
-            SendEvent(syncSendto, "_Sync", isOn, JoinSync.Latest);
+            var syncSendto = globalActiveSwitch ? SendTo.All : SendTo.Self;
+            var evObj = SendEvent(syncSendto, "_Sync", isOn);
+            if( syncSendto == SendTo.All ) SetJoinEvent(evObj);
 
-            var playSendto = isGlobalSound ? SendTo.All : SendTo.Self;
+            var playSendto = globalSound ? SendTo.All : SendTo.Self;
             SendEvent(playSendto, "_Play", isOn);
 
             ExcuteEvent();
@@ -240,7 +250,7 @@ namespace tutinoco
 
         public void ExcuteEvent()
         {
-            var sendto = isGlobalEvent ? SendTo.All : SendTo.Self;
+            var sendto = globalEvent ? SendTo.All : SendTo.Self;
             if( isOn && onEventName!="" ) SendEvent(sendto, onEventName, onEventArgs, onEventTarget);
             if( !isOn && offEventName!="" ) SendEvent(sendto, offEventName, offEventArgs, offEventTarget);
         }
@@ -255,8 +265,20 @@ namespace tutinoco
             }
 
             if( name == "_Play" && audioSource!=null ) {
-                var sound = isOn ? onSound : offSound;
+                bool flg = GetBool();
+                var sound = flg ? onSound : offSound;
                 if( sound!=null ) audioSource.PlayOneShot(sound);
+                if( particle != null && playParticleWithSound && !(type == SwitchType.Button && !flg) ) particle.Play();
+            }
+
+            if( name == "Portal" ) {
+                GameObject target = null;
+                foreach(var b in GetBehaviours(group) ) if( b.gameObject != gameObject ) { target = b.gameObject; break; }
+                if( target == null ) return;
+                var pos = target.transform.position + me.GetPosition() - transform.position;
+                Quaternion rot = target.transform.rotation * Quaternion.Inverse(transform.rotation) * me.GetRotation();
+                //rot = me.GetRotation();
+                me.TeleportTo(pos, rot);
             }
         }
     }
